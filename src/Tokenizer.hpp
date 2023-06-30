6557b54ad0d9480aed95a7ed96f3fecb3f831d40
janniks mac <jannik.loth@gmx.de>
2023-07-01 00:08:08 +0200

    optimized and beautified tokenizer code

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -10,93 +10,74 @@
 
 using namespace std;
 
-class Tokenizer 
+class Tokenizer
 {
- 
+
     string src;
 
 public:
     // overloaded constructor
-    Tokenizer(string s) 
-	{
-        src = s + " "; // Das Leerzeichen dient dazu, beim Lesen von Zahlen nicht 
-					   // über die String-Grenze zu laufen
+    Tokenizer(string s)
+    {
+        src = s + " "; // Das Leerzeichen dient dazu, beim Lesen von Zahlen nicht
+        // über die String-Grenze zu laufen
     }
 
     // splits a string into tokens
     // return type is a vector of pointers to tokens
-    vector<Token*>* tokenize() 
-	{
+    vector<Token *> *tokenize()
+    {
         // vector that saves the single tokens as strings
         vector<string> stringVector;       //Testing purposes
 
         // given code from the exercise
-        vector<Token*>* tokens = new vector<Token*>();
-	    vector<Token*>::iterator i = tokens->begin();
+        vector<Token *> *tokens = new vector<Token *>();
+        vector<Token *>::iterator i = tokens->begin();
 
         // length of the string for the loop
         int stringLength = src.length();
 
 
-        // iterate over the string
-        for (int i = 0; i < stringLength; i++){
+        // iterate over every single char in the string
+        for (int i = 0; i < stringLength; i++)
+        {
 
-            // check if the current char is a bracket
-            if (src[i] == '(' || src[i] == ')' ){
-                stringVector.push_back(src.substr(i, 1));       //Testing purposes
-
-                // push a new Bracket object to the vector
+            // check if the current char is a bracket and push a new Bracket object to the vector
+            if (src[i] == '(' || src[i] == ')')
+            {
                 tokens->push_back(new Bracket(src[i]));
+
+                cout << "Bracket: " << src[i] << endl;
             }
 
-            // check if the current char is an operator
-            else if (src[i] == '+' || src[i] == '-' || src[i] == '*' || src[i] == '/') {
-                stringVector.push_back(src.substr(i, 1));       //Testing purposes
-
-                // push a new Op object to the vector
+            // check if the current char is an operator (+, -, *, /) and push a new Op object to the vector
+            else if (src[i] == '+' || src[i] == '-' || src[i] == '*' || src[i] == '/')
+            {
                 tokens->push_back(new Op(src[i]));
+
+                cout << "Operator: " << src[i] << endl;
             }
 
-            // check if the current char is a number
-            else if (src[i] <= '9' && src[i] >= '0'){
+            // check if the current char is a number (0-9) and push a new Num object to the vector
+            else if (isdigit(src[i]))
+            {
+                int number = atoi(src.substr(i, stringLength).c_str());
+                tokens->push_back(new Num(number));
 
-                int j = i;
-                vector<char> numVector;
+                cout << "Number: " << number << endl;
 
-                // put all single digits of the number into a vector
-                while (src[j] <= '9' && src[j] >= '0'){
-                    numVector.push_back(src[j]);
-                    j++;
-                }
-
-                int builtNumber = 0;
-
-                // bult the number from the vector of single digits
-                for (int k = 0; k < numVector.size(); k++){
-                    int temp = numVector[k] - '0';
-                    builtNumber += temp * pow(10, numVector.size() - k - 1);
-                }
-
-                // set counter i to the right value to continue the loop
-                j = j-i;
-                i += j-1;
-
-                stringVector.push_back(to_string(builtNumber));       //Testing purposes
-
-                // push a new Num object to the vector
-                tokens->push_back(new Num(builtNumber));
-
+                i += log10(number);
             }
         }
 
         // print out vector tokens for testing purposes
         cout << "Testausgabe: " << endl;
         cout << "Einzelne Tokens: " << endl;
-        for (int i = 0; i < stringVector.size(); i++){
-            cout << stringVector[i] << "  //  ";
+        for (int i = 0; i < stringVector.size(); i++) {
+            cout << stringVector[i] << endl;
         }
 
         return tokens;
     }
-    
+
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+
+    for(int i = 1; i < 1000000; i*=10)
+    {
+        cout << "i=" << i << "  log10=" << log10(i) << endl;
+    }
+
     string exp = "((10 + 20) * 30)";
 
     Tokenizer *t = new Tokenizer(exp);
