76fdeed3596dcb4442b3fcc98134c6429d155237
janniks mac <jannik.loth@gmx.de>
2023-07-06 00:03:10 +0200

    splitted Tokenizer.hpp.hpp into .hpp/.cpp

---
+++ src/Tokenizer.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Jannik Loth on 05.07.23.
+//
+
+#include "Tokenizer.hpp"
+
+// splits a string into tokens from type Number, Operator and Bracket
+vector<Token *> *Tokenizer::tokenize()
+{
+    // vector that saves the single tokens as strings
+    vector<string> stringVector;       //Testing purposes
+
+    // given code from the exercise
+    vector<Token *> *tokens = new vector<Token *>();
+    vector<Token *>::iterator i = tokens->begin();
+
+    // length of the string for the loop
+    int stringLength = m_src.length();
+
+    // iterate over every single char in the string
+    for (int i = 0; i < stringLength; i++)
+    {
+
+        // check if the current char is a bracket and push a new Bracket object to the vector
+        if (m_src[i] == '(' || m_src[i] == ')')
+        {
+            tokens->push_back(new Bracket(m_src[i]));
+        }
+
+            // check if the current char is an operator (+, -, *, /) and push a new Operator object to the vector
+        else if (m_src[i] == '+' || m_src[i] == '-' || m_src[i] == '*' || m_src[i] == '/')
+        {
+            tokens->push_back(new Operator(m_src[i]));
+            Operator::incNodes();
+        }
+
+            // check if the current char is a m_number (0-9) and push a new Number object to the vector
+        else if (isdigit(m_src[i]))
+        {
+            int number = atoi(m_src.substr(i, stringLength).c_str());
+            tokens->push_back(new Number(number));
+
+            Operator::incNodes();
+
+            // increase i by the length of the number
+            i += log10(number);
+        }
+    }
+    return tokens;
+}
--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -1,3 +1,6 @@
+#ifndef ARITHMETIC_TREE_TOKENIZER_HPP
+#define ARITHMETIC_TREE_TOKENIZER_HPP
+
 #include <string>
 #include <vector>
 
@@ -12,65 +15,20 @@ using namespace std;
 
 class Tokenizer
 {
-
-    string src;
-
 public:
     // overloaded constructor
     Tokenizer(string s)
     {
-        src = s + " "; // Das Leerzeichen dient dazu, beim Lesen von Zahlen nicht
+        m_src = s + " "; // Das Leerzeichen dient dazu, beim Lesen von Zahlen nicht
         // über die String-Grenze zu laufen
     }
 
-    // splits a string into tokens
-    // return m_type is a vector of pointers to tokens
-    vector<Token *> *tokenize()
-    {
-        // vector that saves the single tokens as strings
-        vector<string> stringVector;       //Testing purposes
+    /// splits a string into tokens from type Number, Operator and Bracket
+    /// @return vector<Token*>: the vector of tokens
+    vector<Token *> *tokenize();
 
-        // given code from the exercise
-        vector<Token *> *tokens = new vector<Token *>();
-        vector<Token *>::iterator i = tokens->begin();
-
-        // length of the string for the loop
-        int stringLength = src.length();
-
-
-        // iterate over every single char in the string
-        for (int i = 0; i < stringLength; i++)
-        {
-
-            // check if the current char is a bracket and push a new Bracket object to the vector
-            if (src[i] == '(' || src[i] == ')')
-            {
-                tokens->push_back(new Bracket(src[i]));
-            }
-
-            // check if the current char is an operator (+, -, *, /) and push a new Operator object to the vector
-            else if (src[i] == '+' || src[i] == '-' || src[i] == '*' || src[i] == '/')
-            {
-                tokens->push_back(new Operator(src[i]));
-                Operator::incNodes();
-
-                //cout << "Operator: " << src[i] << endl;
-            }
-
-            // check if the current char is a m_number (0-9) and push a new Number object to the vector
-            else if (isdigit(src[i]))
-            {
-                int number = atoi(src.substr(i, stringLength).c_str());
-                tokens->push_back(new Number(number));
-
-                Operator::incNodes();
-
-                // increase i by the length of the number
-                i += log10(number);
-            }
-        }
-
-        return tokens;
-    }
-
+private:
+    string m_src;
 };
+
+#endif //ARITHMETIC_TREE_TOKENIZER_HPP
