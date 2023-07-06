55d6e4568083805a673f6889d017a04b2cab2276
daimlereddi <carlf2004@web.de>
2023-07-06 15:43:21 +0200

    added an argument check

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,38 @@ static int node_counter;
 int main(int argc, char* argv[])
 {
     string input = " ";
+    char mode = 0;
+    Evaluator e;
 
+    // test strings
     string infix = "((10 + 20) * 30)";
     string infix2 = "( ( (2 + 2) + (2 + 2) ) * ( (10 / 5) * 3) )";
     string postfix = "10 20 + 30 *";
     string prefix = "+ 10 20";
 
-    if (argc > 1) input = argv[1];
-    for (int i = 1; i < argc; i++)
-    {
-        input += " ";
+    // check if enough arguments are given
+    if (argc < 3) {
+        std::cout << "Zu wenige Argumente angegeben." << std::endl;
+        return 1;
+    }
+
+    // read the input string and the mode
+    // spacings are allowed in the input string
+    for (int i = 1; i < argc - 1; i++) {
         input += argv[i];
     }
-    if (input.length() == 0)
-        cout << "Arguments needed!" << endl;
-    else
-    {
-        char mode = argv[1][0];
-        if (mode == '<' || mode == '|' || mode == '>')
-            input = input.substr(1);
-        else
-            mode = '|'; // Standard: Infix
-        Evaluator e;
-        e.evaluate(input, mode);
-    }
+    mode = argv[argc - 1][0];
+
+    // print out the arguments
+    std::cout << "Rechenausdruck: " << input << std::endl;
+    std::cout << "Zeichen: " << mode << std::endl;
+
+    // evaluate the expression
+
+    e.evaluate(input, mode);
 
 
-    //Tokenizer *t = new Tokenizer(exp);
+    // Test cases for test strings
     //e.evaluate(prefix, '<');
     //e.evaluate(infix2, '|');
     //e.evaluate(postfix, '>');
