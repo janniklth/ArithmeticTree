baa908c6b367563faf617d4c0aab17f68932d8ca
janniks mac <jannik.loth@gmx.de>
2023-07-06 10:55:46 +0200

    beautyfied code and comments

--- src/Token Classes/Bracket.hpp
+++ src/Token Classes/Bracket.hpp
@@ -4,60 +4,54 @@
 #include <string>
 #include "Token.hpp"
 
-/*
- * Token zur Darstellung einer Klammer.
- */
+
+/// Bracket node class
+/// @brief node class representing a bracket
 class Bracket : public Token 
 {
-
 public:
-
-    /*
-     * Erzeugt einen neuen Klammer-Token.
-     * 
-     * Parameter: t Typ der Klammer: '(' oder ')'
-     */
-    Bracket(char s)
+    /// overloaded constructor, creates a new bracket node with the given value
+    /// @param bracket: the value of the bracket node (´(´ or ´)´)
+    Bracket(char bracket_value)
     {
         m_tokenType = TokenType::BRACKET;
-        m_value = s;
+        m_value = bracket_value;
     }
 
-    /*
-     * Dummy-Methode, tut nichts.
-     * 
-     * Return: 0
-     */
-    int eval()
+    /// Dummy-method, does nothing
+    /// @return int: 0
+    /// TODO: evaluate if its better to make the b ase function not pure virtual
+    int eval() override
     {
         return 0;
     }
 
-    /*
-     * Dummy-Methode, tut nichts.
-     * 
-     * Return: leerer String
-     */
-    string prefix() { return ""; }
+    /// Dummy-method, does nothing
+    /// @return string: empty string
+    /// TODO: evaluate if its better to make the b ase function not pure virtual
+    string prefix() override
+    {
+        return "";
+    }
 
-    /*
-     * Dummy-Methode, tut nichts.
-     * 
-     * Return: leerer String
-     */
-    string infix() { return ""; }
+    /// Dummy-method, does nothing
+    /// @return string: empty string
+    /// TODO: evaluate if its better to make the b ase function not pure virtual
+    string infix() override
+    {
+        return "";
+    }
 
-    /*
-     * Dummy-Methode, tut nichts.
-     * 
-     * Return: leerer String
-     */
-    string postfix() { return ""; }
+    /// Dummy-method, does nothing
+    /// @return string: empty string
+    /// TODO: evaluate if its better to make the b ase function not pure virtual
+    string postfix() override
+    {
+        return "";
+    }
 
-    /*
-     * Dummy-Methode, tut nichts.
-     * 
-     * Parameter: o Dummy-Parameter
-     */
+    /// Dummy-method, does nothing
+    /// @param o: dummy parameter
+    /// TODO: evaluate if its better to make the b ase function not pure virtual
     void order(Order o) {}
 };
