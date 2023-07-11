5d27fec7262c393b9a8c2ecb713495564f59f2a9
janniks mac <jannik.loth@gmx.de>
2023-07-11 20:11:14 +0200

    arg parsing with bracking counting working

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,13 @@ using namespace std;
 // static var to count the nodes
 static int node_counter;
 
-// function to parse arguments and check if all arguments are given
-bool parse_arguments(int argc, char *argv[], char *mode, string *input)
+/// function to parse arguments and check if all arguments are given
+/// @param argc: number of arguments
+/// @param argv: list of arguments
+/// @param mode: pointer to the mode var
+/// @param expression: pointer to the input var
+/// @return: true if all arguments are given, false if not
+bool parse_arguments(int argc, char *argv[], char *mode, string *expression)
 {
     // initialise a var to store if the parsing was successful
     bool is_successful = true;
@@ -32,43 +37,70 @@ bool parse_arguments(int argc, char *argv[], char *mode, string *input)
     }
 
     // check if all arguments are given, if not throw a specific error an exit
-    if (!argument_m) { cout << "ERROR: argument -m for the mode is missing" << endl; is_successful = false;}
-    if (!argument_e) { cout << "ERROR: argument -e for the expression is missing" << endl; is_successful = false;}
-    if (!is_successful) {return is_successful;};
+    if (!argument_m) {
+        cout << "ERROR: argument -m for the mode is missing" << endl;
+        is_successful = false;
+    }
+    if (!argument_e) {
+        cout << "ERROR: argument -e for the expression is missing" << endl;
+        is_successful = false;
+    }
+    if (!is_successful) { return is_successful; };
 
     // loop over all arguments to read the arguments
     for (int i = 0; i < argc - 1; i++) {
 
+        int bracket_counter = 0;
+        bool bracket_counting_successful = false;
+
         // check for expression
         if (!strcmp(argv[i], "-e")) {
-            // put argument in the input var until an "-m"
-            while (strcmp(argv[i + 1], "-m") != 0) {
-                *input += argv[i + 1];
+            // concatenate all arguments until all brackets are closed
+            do {
+                for (int j = 0; j < strlen(argv[i + 1]); j++) {
+                    // check if the next charachter is a opening bracket
+                    if (argv[i + 1][j] == '(') {
+                        bracket_counter++;
+                        bracket_counting_successful = true;
+                    }
+
+                        // check if the next charachter is a closing bracket
+                    else if (argv[i + 1][j] == ')') {
+                        bracket_counter--;
+                        bracket_counting_successful = true;
+                    }
+
+                    // concatenate the charachter to the expression
+                    *expression += argv[i + 1][j];
+                }
+                // step to the next argument
                 i++;
+            } while (bracket_counter != 0);
+
+            // check if the expression is empty or ni brackets where found
+            if (!bracket_counting_successful) {
+                cout
+                        << "ERROR: argument for expression (-e) is not correct. "
+                        << " \nPlease use -e <expression> and expression: ((1+2)*3), all-encompassing brackets are important!"
+                        << endl;
+                is_successful = false;
             }
         }
 
-        // check for mode
-        else if (!strcmp(argv[i], "-m"))
-        {
-            if (!strcasecmp(argv[i + 1], "PREFIX"))
-            {
+            // check for mode
+        else if (!strcmp(argv[i], "-m")) {
+            if (!strcasecmp(argv[i + 1], "PREFIX")) {
                 *mode = '<';
                 i++;
-            }
-            else if (!strcasecmp(argv[i + 1], "INFIX"))
-            {
+            } else if (!strcasecmp(argv[i + 1], "INFIX")) {
                 *mode = '|';
                 i++;
-            }
-            else if (!strcasecmp(argv[i + 1], "POSTFIX"))
-            {
+            } else if (!strcasecmp(argv[i + 1], "POSTFIX")) {
                 *mode = '>';
                 i++;
-            }
-            else
-            {
-                cout << "ERROR: argument for mode (-m) is not correct: " <<  argv[i + 1] << endl;
+            } else {
+                cout << "ERROR: argument for mode (-m) is not correct: " << argv[i + 1]
+                     << " please use -m <mode> and mode: infix, prefix or postfix" << endl;
                 is_successful = false;
             }
         }
@@ -76,18 +108,18 @@ bool parse_arguments(int argc, char *argv[], char *mode, string *input)
     return is_successful;
 }
 
-
-int main(int argc, char* argv[])
+// main loop
+int main(int argc, char *argv[])
 {
-    #if defined(WIN32) || defined(WIN64)
-         SetConsoleOutputCP(CP_UTF8);
-    #endif
+#if defined(WIN32) || defined(WIN64)
+    SetConsoleOutputCP(CP_UTF8);
+#endif
 
     Evaluator e;
     string input = " ";
     char mode = 0;
 
-    if (!(parse_arguments(argc, argv, &mode, &input))){
+    if (!(parse_arguments(argc, argv, &mode, &input))) {
         cout << "ERROR: parsing arguments failed";
         return 1;
     }
